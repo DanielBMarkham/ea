683383da30e66a596b38067b5ecaec62f4a637f8
Daniel Markham <danielbmarkham@hotmail.com>
2018-10-07 14:11:37 -0400

    hmm

--- system/structure/src/AppCommon/EATypeExtensions.fs
+++ system/structure/src/AppCommon/EATypeExtensions.fs
@@ -2,3 +2,9 @@ module EATypeExtensions
 open SystemTypeExtensions
 open SystemUtilities
 open CommandLineHelper
+open System
+
+type CompilationUnit = {
+    Info:System.IO.FileInfo
+    FileContents:string[]
+}
