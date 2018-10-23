ac2a5295b2fd086e0ede6b748b3ab6768b42956a
Daniel Markham <DanielBMarkham@Hotmail.com>
2018-10-23 04:37:50 -0400

    Bonus compile

--- system/structure/src/ea/Program.fs
+++ system/structure/src/ea/Program.fs
@@ -20,42 +20,43 @@
     /// It answers the question: can you run at all?
     [<EntryPoint>]
     let main argv =
-        // if there's a file coming from the O/S, get all of it, right now
-        // downstream they'll exepct a sequence, and that's the best thing to
-        // use, but we need to free up resources to clarify any threading issues
-        // so we'll fake out a seq for now
-        // * Piping continued to be fun. Worked on it right up until last tests were passing
-        // * Each environment acted differently. Yay!
-        let incomingFileStream=
-          try
-            if System.Console.IsInputRedirected && System.Console.KeyAvailable && System.Console.In.Peek() <>(-1) // && System.Console.KeyAvailable
-                then
-                    let getAll=Seq.initInfinite(fun _ -> Console.ReadLine()) |>  Seq.takeWhile (fun line -> line <> null)
-                    let newVal=getAll |> Seq.toArray |> Array.toSeq
-                    newVal
-                else
-                    []:>seq<string>
-            //with |ex->[ex.Message]:>seq<string>
-            with |ex->[]:>seq<string>
-        logEvent Verbose "Method main beginning....." moduleLogger
-        use mre = new System.Threading.ManualResetEventSlim(false)
-        use sub = Console.CancelKeyPress.Subscribe (fun _ -> mre.Set())
+      // if there's a file coming from the O/S, get all of it, right now
+      // downstream they'll exepct a sequence, (a list comprehension even better) 
+      // and those are the best thing to
+      // use, but we need to free up resources to clarify any threading issues
+      // so we'll fake out a seq for now
+      // * Piping continued to be fun. Worked on it right up until last tests were passing
+      // * Each environment acted differently. Yay!
+      let incomingFileStream=
+        try
+          if System.Console.IsInputRedirected && System.Console.KeyAvailable && System.Console.In.Peek() <>(-1) // && System.Console.KeyAvailable
+              then
+                let getAll=Seq.initInfinite(fun _ -> Console.ReadLine()) |>  Seq.takeWhile (fun line -> line <> null)
+                let newVal=getAll |> Seq.toArray |> Array.toSeq
+                newVal
+              else
+                []:>seq<string>
+          //with |ex->[ex.Message]:>seq<string>
+        with |ex->[]:>seq<string>
+      logEvent Verbose "Method main beginning....." moduleLogger
+      use mre = new System.Threading.ManualResetEventSlim(false)
+      use sub = Console.CancelKeyPress.Subscribe (fun _ -> mre.Set())
 
-        let cts = new CancellationTokenSource()
+      let cts = new CancellationTokenSource()
 
-        //Console.Out.WriteLine "Press Ctrl-C to terminate program"
-        // As long as we're a single-thread on a console app, we can
-        // use a mutable cell for a return value, allowing whatever
-        // threading mechanis m we use for everything else the job of returning an int back
-        // to send to the O/S (In other words, this is a special case,
-        // if you start spinning threads off directly in main or newMain,
-        // this will not work.)
-        // * Mutable variables FTW! I think.
-        let mutable ret=0
-        // This needs to be a sequence since it represents possibly realtime streams of strings arriving
-        let incomingFileStreams =
-            if (incomingFileStream |> Seq.length >0) then incomingFileStream else Seq.empty
-        newMain argv cts mre (incomingFileStreams) &ret
-        mre.Wait(cts.Token)
-        ret
-    // Can't log the module exit. There's nothing to log to
+      //Console.Out.WriteLine "Press Ctrl-C to terminate program"
+      // As long as we're a single-thread on a console app, we can
+      // use a mutable cell for a return value, allowing whatever
+      // threading mechanis m we use for everything else the job of returning an int back
+      // to send to the O/S (In other words, this is a special case,
+      // if you start spinning threads off directly in main or newMain,
+      // this will not work.)
+      // * Mutable variables FTW! I think.
+      let mutable ret=0
+      // This needs to be a sequence since it represents possibly realtime streams of strings arriving
+      let incomingFileStreams =
+          if (incomingFileStream |> Seq.length >0) then incomingFileStream else Seq.empty
+      newMain argv cts mre (incomingFileStreams) &ret
+      mre.Wait(cts.Token)
+      ret
+  // Can't log the module exit. There's nothing to log to
